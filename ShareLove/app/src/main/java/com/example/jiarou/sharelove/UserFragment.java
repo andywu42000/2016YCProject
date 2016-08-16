21328e0c0a88d658be249ecb7434e860759d14ed
joyshao37 <102306001@nccu.edu.tw>
2016-08-16 21:00:13 +0800

    匯入樂透頁面、樂透頁面可跳轉到搜尋頁面

--- ShareLove/app/src/main/java/com/example/jiarou/sharelove/UserFragment.java
+++ ShareLove/app/src/main/java/com/example/jiarou/sharelove/UserFragment.java
@@ -34,7 +34,7 @@ public class UserFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     String imgurURL = "http://i.imgur.com/";
-    Button coupon_btn;
+    Button coupon_btn, lottery_btn;
 
 
     // TODO: Rename and change types of parameters
@@ -96,6 +96,18 @@ public class UserFragment extends Fragment {
             public void onClick(View v) {
                 final Intent intent = new Intent();
                 intent.setClass(getActivity(), CouponMainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        //跳到 Lottery的按鈕
+        lottery_btn=(Button)view.findViewById(R.id.lottery_btn);
+        lottery_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent();
+                intent.setClass(getActivity(), LotteryMainActivity.class);
                 startActivity(intent);
 
             }
